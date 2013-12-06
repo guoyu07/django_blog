fafd820c65434bd00baed973bcdef4fb74c34470
Henry Lee <manageyp@gmail.com>
2013-12-06 13:43:39 +0800

    [Change] add missing models

---
+++ blog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=True)
+
+    def __unicode__(self):
+        return u'%s' %(self.name)
+
+    class Meta:
+        db_table = "authors"
+
+
+class Article(models.Model):
+    author = models.ForeignKey(Author)
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=True)
+
+    def __unicode__(self):
+        return u'%s' %(self.name)
+
+    class Meta:
+        db_table = "articles"
