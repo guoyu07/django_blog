3e72acdee8b14a01adcd3ca99d9da3af0e6d02d8
Henry Lee <manageyp@gmail.com>
2014-01-16 15:04:59 +0800

    [NewFeature] config memcached

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -94,6 +94,19 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Memcached
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+        'TIMEOUT': 60,
+        'OPTIONS': {
+            'MAX_ENTRIES': 1000
+        }
+    }
+}
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
